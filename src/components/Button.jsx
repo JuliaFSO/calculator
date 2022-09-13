7ff5371cdf3c39ffde4c0810c6be3fefa90f5c8e
JuliaFSO <juliafso@gmail.com>
2022-09-13 18:23:09 -0400

    Calculator using React

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Button.css"
+
+export default props => {
+  return (
+    <button
+    onClick={e => props.click && props.click(props.label)}
+    className={`
+      button
+      ${props.operation ? 'operation' : ''}
+      ${props.double ? 'double' : ''}
+      ${props.triple ? 'triple' : ''}
+    `}>
+      {props.label}
+    </button>
+  )
+}
